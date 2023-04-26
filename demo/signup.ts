5451ff13bc6b94afefd87f05768ece1022ac1496
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-04-26 10:30:36 +0800

    add signup form demo

---
+++ demo/signup.ts
@@ -0,0 +1,71 @@
+import { appendChild, text, watch } from '../core'
+import { div, form, input, label, p, br, img } from '../helpers'
+import { selectImage } from '@beenotung/tslib/file'
+import { compressMobilePhoto } from '@beenotung/tslib/image'
+
+let usernameInput = input({ id: 'username' })
+let passwordInput = input({ id: 'password', type: 'password' })
+let confirmPasswordInput = input({ id: 'confirm-password', type: 'password' })
+let avatarInput = input({
+  id: 'avatar',
+  type: 'button',
+  value: 'choose an image',
+  onclick: selectAvatar,
+})
+let avatarImg = img({ alt: 'avatar preview' })
+avatarImg.style.maxWidth = '100%'
+avatarImg.style.maxHeight = '50vh'
+
+let previewText = text()
+
+let isValid = false
+
+watch(() => {
+  previewText.textContent = usernameInput.value + ':' + passwordInput.value
+})
+
+watch(() => {
+  isValid = passwordInput.value == confirmPasswordInput.value
+  let color = isValid ? 'green' : 'red'
+  confirmPasswordInput.style.outline = '3px solid ' + color
+})
+
+function inputField(input: HTMLInputElement) {
+  let inputFieldDiv = div({ className: 'input-field' }, [
+    label({ textContent: input.id + ': ', htmlFor: input.id }),
+    br(),
+    input,
+  ])
+  inputFieldDiv.style.marginBottom = '0.5rem'
+  input.style.marginTop = '0.25rem'
+  return inputFieldDiv
+}
+
+async function selectAvatar() {
+  console.log('selectAvatar')
+  let [file] = await selectImage()
+  if (!file) return
+  let dataUrl = await compressMobilePhoto({ image: file })
+  avatarImg.src = dataUrl
+}
+
+function submitForm(event: Event) {
+  event.preventDefault()
+  if (!isValid) {
+    alert('please correct the fields before submitting')
+    return
+  }
+  alert('todo')
+}
+
+let signupForm = form({ onsubmit: submitForm }, [
+  inputField(usernameInput),
+  inputField(avatarInput),
+  avatarImg,
+  inputField(passwordInput),
+  inputField(confirmPasswordInput),
+  p({}, ['preview: ', previewText]),
+  input({ type: 'submit', value: 'Sign Up' }),
+])
+
+appendChild(document.body, signupForm)
