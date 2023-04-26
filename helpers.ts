d1c53ec5ab70e46a92797bcba2b47b9f35cd5b19
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-04-26 10:30:05 +0800

    add helper creation functions for div and form

--- helpers.ts
+++ helpers.ts
@@ -3,6 +3,8 @@ import { genCreateHTMLElement } from './core'
 export const br = genCreateHTMLElement('br')
 export const p = genCreateHTMLElement('p')
 export const a = genCreateHTMLElement('a')
+export const div = genCreateHTMLElement('div')
+export const form = genCreateHTMLElement('form')
 export const input = genCreateHTMLElement('input')
 export const label = genCreateHTMLElement('label')
 export const span = genCreateHTMLElement('span')
